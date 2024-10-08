911d825b9457d3b07378b00d3fffe645cf699bb1
Clarence Jordan <clarencejordan087@gamil.com.com>
2024-10-08 13:56:53 -0700

    add photos and stoping point

--- src/sections/Rooms.tsx
+++ src/sections/Rooms.tsx
@@ -1,9 +1,22 @@
+import Buttionbg from '@/components/Buttionbg'
 import React from 'react'
 
 const Rooms = () => {
   return (
     <div className='bg-light_beige
-'>Rooms</div>
+'>
+    <div className='w-96 '>
+        <h1 className='text-default_text text-4xl font-semibold py-1'>
+    50+ Beautiful rooms 
+    inspiration
+    </h1>
+    <p className='text-default_text py-3'>
+    Our designer already made a lot of beautiful prototype of rooms that inspire you
+    </p>
+    {/* TODO add link path  */}
+    <Buttionbg href='#' label='Explore More'/>
+    </div>
+    </div>
   )
 }
 
