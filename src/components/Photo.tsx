debbcf89f0abaa26a3d81d238c11660b39d20158
Clarence Jordan <clarencejordan087@gamil.com.com>
2024-10-07 17:28:35 -0700

    add mobile size

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { photo } from '@/lib/types/types'
 const Photo:React.FC<photo> = ({photo,label}) => {
   return (
-    <div className='text-center flex flex-col gap-9'>
+    <div className='text-center flex flex-col gap-9 max-sm:gap-3 max-sm:w-32'>
     <Image
       src={photo}
       width={0}
