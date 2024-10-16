83c671aebe2db2c2a8e68eeb89bc448daa330654
Clarence Jordan <clarencejordan087@gamil.com.com>
2024-10-15 20:36:08 -0500

    add head of page

--- src/components/ShoppingHead.tsx
+++ src/components/ShoppingHead.tsx
@@ -3,16 +3,23 @@ import Image from "next/image";
 import photo from "../../public/assets/Rectangle 1.jpg";
 const ShoppingHead = () => {
   return (
-    <>
+    <div className="flex justify-center items-center w-full">
       {" "}
       <Image
         src={photo}
         alt="background" // Use the title as alt text for better accessibility
         objectFit="fill"
-          className="w-full"
+        className="w-full"
         layout="cover"
       />
-    </>
+      <div className="z-10 absolute flex flex-col justify-center gap-3">
+        <h1 className="text-5xl font-semibold ">Shop</h1>
+        <div className="flex gap-2">
+          <strong>Home &gt;</strong>
+          <p>Shop</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
